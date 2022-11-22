8d19664f1cf82d4f07d4a9197b616b4350ae38a2
celsuss <jenslorden@gmail.com>
2022-11-22 23:46:22 +0100

    Started setting up docker images for api and db

---
+++ main.py
@@ -0,0 +1,8 @@
+import tensorflow_recommenders as tfrs
+import tensorflow_datasets as tfds
+from typing import Dict, Text
+import tensorflow as tf
+import numpy as np
+import tempfile
+import pprint
+import os
