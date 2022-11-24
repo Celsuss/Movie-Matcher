72db1826e6ed413cccf61984f7a24d80f07b6283
Jens <jenslorden@gmail.com>
2022-11-24 23:01:14 +0100

    Some refactoring

--- api/app.py
+++ api/app.py
@@ -1,17 +1,1 @@
-from flask import Flask
-from flask_cors import CORS
-from flask_sqlalchemy import SQLAlchemy
-import os
-
-app = Flask(__name__)
-CORS(app)
-
-host = os.environ['DB_HOST']
-# app.config["SQLALCHEMY_DATABASE_URI"] = "postgres://{}".format(host)
-app.config["SQLALCHEMY_DATABASE_URI"] = "postgresql://{}".format(host)
-app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
-db = SQLAlchemy(app)
-
-@app.route('/')
-def hello():
-    return 'My First API !!'
+from api import db, models
