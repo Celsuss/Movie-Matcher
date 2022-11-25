b6eeb85bed674e51c457b4f00edf91ff1d55619f
Jens <jenslorden@gmail.com>
2022-11-25 19:42:36 +0100

    Added greaphql schema file

---
+++ api/api/__init__.py
@@ -0,0 +1,24 @@
+from flask import Flask
+from flask_cors import CORS
+from flask_sqlalchemy import SQLAlchemy
+import os
+
+app = Flask(__name__)
+CORS(app)
+
+# host = os.environ['DB_HOST']
+# port = os.environ['DB_PORT']
+# user = os.environ['DB_USER']
+# password = os.environ['DB_PASSWORD']
+# name = os.environ['DB_NAME']
+host = 'host'
+port = 1111
+user = 'user'
+
+app.config["SQLALCHEMY_DATABASE_URI"] = "postgresql://{}:{}/".format(host, port, user)
+app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+db = SQLAlchemy(app)
+
+@app.route('/')
+def hello():
+    return 'My First API !!'
--- api/app.py
+++ api/app.py
@@ -1,27 +1,6 @@
-from flask import Flask
-from flask_cors import CORS
-from flask_sqlalchemy import SQLAlchemy
-import os
+from api import app, db
+from api import models
 
-app = Flask(__name__)
-CORS(app)
-
-# host = os.environ['DB_HOST']
-# port = os.environ['DB_PORT']
-# user = os.environ['DB_USER']
-# password = os.environ['DB_PASSWORD']
-# name = os.environ['DB_NAME']
-host = 'host'
-port = 1111
-user = 'user'
-
-app.config["SQLALCHEMY_DATABASE_URI"] = "postgresql://{}:{}/".format(host, port, user)
-app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
-db = SQLAlchemy(app)
-
-@app.route('/')
-def hello():
-    return 'My First API !!'
 
 
 if __name__ == '__main__':
